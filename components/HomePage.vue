3eb76ea2819ee1b3e7b4049c30ae8797917fbe47
retraigo <nett@nekooftheabyss.xyz>
2022-03-13 11:36:46 +0530

    fonts

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="flex flex-col items-center space-y-1">
-    <div class="font-bold text-2xl text-red-700 font-cursive">
+  <div class="flex flex-col items-center space-y-1 font-monsterrat">
+    <div class="font-semibold text-xl text-red-700 font-montserrat">
       Easwari Engineering College (Autonomous)
     </div>  
-    <div class="font-cursive font-bold text-2xl">
+    <div class="font-montserrat font-bold text-2xl text-purple-700">
       Department Of Artificial Intelligence & Data Science
     </div>
     <div class="">Proudly Welcomes You To</div>
-    <div class="font-cursive font-bold text-red-500 transition transform duration-500 ease-in-out hover:scale-110 text-4xl">
+    <div class="font-montserrat font-bold text-blue-800 transition transform duration-500 ease-in-out hover:scale-110 text-2xl">
       International Virtual Conference on Machine Learning Applications in
       Applied Sciences and Mathematics
     </div>
-    <div class="font-semibold">IVCMASM 2022</div>
+    <div class="font-semibold text-blue-500">IVCMASM 2022</div>
     <div class="">(Online Conference)</div>
-    <div class="font-cursive font-semibold text-2xl">March 30th - 31st, 2022</div>
+    <div class="text-red-500 font-montserrat font-semibold text-2xl">March 30th - 31st, 2022</div>
   </div>
 </template>
