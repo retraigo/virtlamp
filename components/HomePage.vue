af9c91613ea45d7413f04567a0b1364dee61f319
retraigo <nett@nekooftheabyss.xyz>
2022-03-13 11:06:55 +0530

    font color

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col items-center space-y-1">
-    <div class="font-bold text-2xl font-cursive">
+    <div class="font-bold text-2xl text-red-700 font-cursive">
       Easwari Engineering College
     </div>  
     <div class="font-cursive font-bold text-2xl">
       Department Of Artificial Intelligence & Data Science
     </div>
     <div class="">Proudly Welcomes You To</div>
-    <div class="font-cursive font-bold text-blue-700 dark:text-blue-300 transition transform duration-500 ease-in-out hover:scale-110 text-4xl">
+    <div class="font-cursive font-bold text-red-500 transition transform duration-500 ease-in-out hover:scale-110 text-4xl">
       International Virtual Conference on Machine Learning Applications in
       Applied Sciences and Mathematics
     </div>
