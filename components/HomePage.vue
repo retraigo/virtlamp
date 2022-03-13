ba3974ba57f55c4236dbb538319419af7c905ce0
retraigo <nett@nekooftheabyss.xyz>
2022-03-13 19:12:46 +0530

    enlarge stuff

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center space-y-1 font-monsterrat">
+  <div class="flex flex-col items-center space-y-0.5 font-monsterrat">
     <div class="font-semibold text-xl text-red-700 font-montserrat">
       Easwari Engineering College (Autonomous)
     </div>  
@@ -11,7 +11,7 @@
       International Virtual Conference on Machine Learning Applications in
       Applied Sciences and Mathematics
     </div>
-    <div class="font-bold tracking-wider text-purple-800">IVCMASM 2022</div>
+    <div class="font-bold text-lg tracking-wider text-purple-800">IVCMASM 2022</div>
     <div class="">(Online Conference)</div>
     <div class="text-red-500 font-montserrat font-semibold text-2xl">March 30th - 31st, 2022</div>
   </div>
