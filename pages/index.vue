9043c7964dbeb50a2b43af2a3537b101e4c07881
retraigo <nett@nekooftheabyss.xyz>
2022-03-25 20:11:01 +0530

    remove overlay

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
 <div>
-  <Overlay />
+<!--  <Overlay /> -->
   <VirtLamp />
 </div>
 </template>
 
 <script>
-import Overlay from '~/components/Overlay.vue'
 export default {
-  components: { Overlay },
   name: 'IndexPage',
 }
 </script>
