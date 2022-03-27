95588b3a540ff5e0682e3dbcb6acd49ed3f47410
retraigo <nett@nekooftheabyss.xyz>
2022-03-27 16:28:15 +0530

    PWA config

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,38 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+
+  // PWA Configuration: https://go.nuxtjs.dev/pwa
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+      icon: {
+        sizes: [16, 32, 64, 120, 144, 152, 192, 384, 512],
+      },
+      manifest: {
+        lang: 'en',
+        name: "International Virtual Conference on Machine Learning Applications in Applied Sciences and Mathematics",
+        short_name: 'IVCMASM 2022',
+        description: 'The International Virtual Conference on Machine Learning Applications in Applied Sciences and Mathematics (IVCMASM 2022). The conference focuses on machine learning applications and latest developments in the Fields of physics, chemistry, mathematics, social sciences, agriculture, economics and other applied sciences. It targets to bring together researchers from all over the world to present the latest research results, and it is one of the main venues for demonstrating the results of research projects in relevant areas. ',
+        background_color: "#ffffff",
+        theme_color: "#fb0100",
+      },
+      meta: {
+        lang: 'en',
+        name: "International Virtual Conference on Machine Learning Applications in Applied Sciences and Mathematics",
+        short_name: 'IVCMASM 2022',
+        description: 'The International Virtual Conference on Machine Learning Applications in Applied Sciences and Mathematics (IVCMASM 2022). The conference focuses on machine learning applications and latest developments in the Fields of physics, chemistry, mathematics, social sciences, agriculture, economics and other applied sciences. It targets to bring together researchers from all over the world to present the latest research results, and it is one of the main venues for demonstrating the results of research projects in relevant areas. ',
+        background_color: "#ffffff",
+        theme_color: "#fb0100",
+        appleStatusBarStyle: 'black',
+        author: "Dept of Artificial Intelligence and Data Science",
+        twitterCard: 'summary_large_image',
+        twitterAuthor: '@retraigo',
+        twitterSite: '@srm_easwari',
+      },
+    },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
